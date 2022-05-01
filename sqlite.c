d79c91d3655a6a3cbd701b68caac3468f051b188
ssangeeeeee <104727451+ssangeeeeee@users.noreply.github.com>
2022-05-01 21:03:58 +0530

    Add files via upload

---
+++ sqlite.c
@@ -0,0 +1,22 @@
+CREATE TABLE MOVIE
+('Mov Name'varchar(30),'Actor'
+varchar(30),'Actress'varchar(30),'Director'
+varchar(30),'yor'DATE);
+INSERT into MOVIE
+VALUE('After',Fiennes Tiffin','Josephine
+Langford','jenny Gage','2019-04-08'),
+('Work it','Jordan Fishe',Sabrina
+carpenter','Laura Terruso','2020-07-07'),
+('Avengers:Enddgame','Robert
+Downey','Scarlett Johanson,''Anthony
+Russo','2019-04-26');
+
+
+
+
+
+SELECT Actor from MOVIE;
+output:
+fiennes Tiffin
+Jordan Fishe
+Robert Downey
